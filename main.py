349f26c413c077ea09932018a416768042f139c6
Brandon Capener <bcapener@gmail.com>
2024-12-22 23:31:42 -0700

    add command line argument

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tempfile
 import os
 import contextlib
 from typing import Generator
+import argparse
 
 
 CURR_DIR = Path(__file__).resolve().parent
@@ -22,44 +23,51 @@ def explode_epub(path: Path):
     assert path.suffix.lower() == ".epub"
     new_path = path.parent / f"{path.stem}_edit{path.suffix}"
 
-    compress_info: dict[str, zipfile.ZipInfo] = {}
     with tempfile.TemporaryDirectory() as temp_dir:
         with zipfile.ZipFile(path, 'r') as zip_ref:
-            compress_info = zip_ref.NameToInfo
+            rel_path_to_zip_info = zip_ref.NameToInfo
             zip_ref.extractall(temp_dir)
 
         yield temp_dir
 
-        full_file_paths = {}
+        rel_path_to_full_path = {}
         for file_path in walk(temp_dir):
             rel_path = str(file_path.relative_to(temp_dir))
-            assert rel_path in compress_info
-            full_file_paths[rel_path] = file_path
+            assert rel_path in rel_path_to_zip_info
+            rel_path_to_full_path[rel_path] = file_path
+
+        # verify no files were added or removed.
+        orig_files = sorted(rel_path_to_zip_info.keys())
+        curr_files = sorted(rel_path_to_full_path.keys())
+        if orig_files != curr_files:
+            raise RuntimeError("No files can be added or deleted from the epub.")
 
         with zipfile.ZipFile(new_path, 'w') as zip_ref:
-            for rel_path, zip_info in compress_info.items():
-                full_path = full_file_paths[rel_path]
+            for rel_path, zip_info in rel_path_to_zip_info.items():
+                full_path = rel_path_to_full_path[rel_path]
                 zip_ref.write(full_path, rel_path, compress_type=zip_info.compress_type)
 
+
+def valid_epub_file(path_str) -> Path:
+    path = Path(path_str).resolve()
+    if not path.exists():
+        raise argparse.ArgumentTypeError(f"Invalid path: {path_str}")
+    if path.suffix.lower() != ".epub":
+        raise argparse.ArgumentTypeError(f"File must have an 'epub' extension. '{path_str}'")
+    return path
+
+
 def main():
-    with explode_epub(CURR_DIR / "Mercy of Gods, The - James S. A. Corey.epub") as epub_dir:
-        for file_path in walk(epub_dir):
-            if file_path.suffix.endswith("html"):
-                content = file_path.read_text()
-                if "fuck" in content:
-                    open(file_path, "w").write(content.replace("fuck", "BEEP"))
-        # for root, dirs, files in os.walk(epub_dir):
-        #     root = Path(root)
-        #     for file in files:
-        #         file_path = root / file
-        #         if file_path.suffix.endswith("html"):
-        #             content = file_path.read_text()
-        #             if "fuck" in content:
-        #                 open(file_path, "w").write(content.replace("fuck", "BEEP"))
-        #             # if "fuck" in file_path.read_text().lower().count("fuck"):
-        #             # if count:
-        #             #     print(file_path, count)
-        #         # print(os.path.relpath(file_path, epub_dir))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=valid_epub_file, help="path to an epub")
+    args = parser.parse_args()
+
+    with explode_epub(args.path) as epub_dir:
+        html_files = (f for f in walk(epub_dir) if "html" in f.suffix)
+        for file_path in html_files:
+            content = file_path.read_text()
+            if "fuck" in content:
+                open(file_path, "w").write(content.replace("fuck", "BEEP"))
 
 
 if __name__ == "__main__":
